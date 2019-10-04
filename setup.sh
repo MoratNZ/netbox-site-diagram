1ae497c572ce156b21abea0ecea051c4ceddcdeb
David Maclagan <dmaclagan@teamtalk.co.nz>
2019-10-04 14:21:36 +1300

    Add setup file to copy python script to netbox scripts dir

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-cp site-diagram.py /usr/local/netbox/netbox/site-diagram.py
+#!/usr/bin/env bash
+  
+echo "Copying script to netbox scripts directory"
+cp site-diagram.py /usr/local/netbox/netbox/scripts/site-diagram.py:w
