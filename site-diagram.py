572b91578f4b8e0b5a8a2ef0ce84b73c0c89951d
David Maclagan <dmaclagan@teamtalk.co.nz>
2019-10-07 13:26:46 +1300

    Commiting debug code

--- site-diagram.py
+++ site-diagram.py
@@ -8,9 +8,14 @@ class SiteDiagram(Script):
     class Meta:
         name = "Site Diagram"
         description = "Generate a topology diagram for a site"
-        fields = ["siteName"]
+        fields = ["site_name", "test_field"]
 
-        siteName = ObjectVar(
+        test_field = StringVar(
+            label="tst",
+            descriptoin "test"
+        )
+        site_name = ObjectVar(
+            label="site",
             description="Site to map",
             queryset=Site.objects.filter()
         )
