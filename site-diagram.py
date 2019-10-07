0501a184228834e75f722d9cb3994f8f87899d4e
David Maclagan <dmaclagan@teamtalk.co.nz>
2019-10-07 13:44:39 +1300

    Retry script input

--- site-diagram.py
+++ site-diagram.py
@@ -7,16 +7,10 @@ class SiteDiagram(Script):
     class Meta:
         name = "Site diagram"
         description = "Draw a logical diagram of a site"
-        fields = ['site_name', 'switch_count', 'switch_model']
+        fields = ['site_code']
 
-    site_name = StringVar(
-        description="Name of the new site"
-    )
-    switch_count = IntegerVar(
-        description="Number of access switches to create"
-    )
-    switch_model = ObjectVar(
-        description="Access switch model",
+    site_code = ObjectVar(
+        description="Site to draw",
         queryset=DeviceType.objects.filter(
             manufacturer__name='Cisco',
         )
